e7604c7ade67ff403143ba5d7ebad14f9c318fb6
niklaspoulsen <32129061+niklaspoulsen@users.noreply.github.com>
2026-03-10 18:37:46 +0100

    cal error

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -4,14 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'plugins_loaded', 'cmbwc_bootstrap' );
-
-function cmbwc_bootstrap() {
-	if ( ! class_exists( 'WooCommerce' ) ) {
-		return;
-	}
-
-	require_once CMBWC_PATH . 'includes/product-admin.php';
-	require_once CMBWC_PATH . 'includes/assets.php';
-	require_once CMBWC_PATH . 'includes/shortcodes.php';
-}
+require_once CMBWC_PATH . 'includes/product-admin.php';
+require_once CMBWC_PATH . 'includes/assets.php';
+require_once CMBWC_PATH . 'includes/shortcodes.php';
